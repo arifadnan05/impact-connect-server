461289c21946594de81ce1d3e6085f4b6bd1e0bb
arifadnan05 <arifahamedadnan@gmail.com>
2024-05-12 00:28:28 +0600

    add volunteer api creted and connected to the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
-require('dotenv').config
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,36 +22,43 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://impactConnect:6v7f3vd3ApeVeCMU@cluster0.8lcgwxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8lcgwxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const jobPostCollection = client.db('impactConnect').collection('jobPost');
+
+        app.post('/add-job-post', async (req, res) => {
+            const user = req.body;
+            const result = await jobPostCollection.insertOne(user)
+            res.send(result)
+
+        })
 
 
 
 
 
 
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -64,7 +71,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    
+
     res.send('Impact connect Server was running...')
 })
 app.listen(port, () => {
