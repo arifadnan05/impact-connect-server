5b1e7131d7ebc8bf26b56184853b7eaf122745b5
arifadnan05 <arifahamedadnan@gmail.com>
2024-05-14 23:33:14 +0600

    cors link updated

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
     origin: ['http://localhost:5173',
         'http://localhost:5174',
-        'https://impact-connect-19304.firebaseapp.com',
-        'https://impact-connect-19304.web.app'],
+        'https://impactconnect.surge.sh',
+        'https://impact-connect-19304.web.app',
+        'https://impact-connect-19304.firebaseapp.com'
+        ],
     credentials: true,
     optionSuccessStatus: 200,
 }
@@ -88,6 +90,17 @@ async function run() {
             const email = req.params.email;
             const query = { loggedInUserEmail: (email) }
             const result = await requestVolunteerCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        // user request job post cancel api
+
+
+        app.delete('/request-job/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await requestVolunteerCollection.deleteOne(query)
             res.send(result)
         })
 
